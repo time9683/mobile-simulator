8281d4edffd2e339c285c44372973f5c6147bea8
HL-2002 <henryslang8231682@gmail.com>
2024-11-01 15:07:36 -0400

    Add onClick support

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,11 +1,12 @@
 interface DialogProps {
     children: React.ReactNode;
     someRef: React.RefObject<HTMLDialogElement>;
+    onClick?: () => void;
 }
 
-export default function Dialog({children, someRef}: DialogProps) {
+export default function Dialog({children, someRef, onClick}: DialogProps) {
     return (
-        <dialog ref={someRef} className='p-6 rounded-lg shadow-md m-auto bg-white'>
+        <dialog ref={someRef} onClick={onClick} className='p-6 rounded-lg shadow-md m-auto bg-white'>
             {children}
         </dialog>
     )
