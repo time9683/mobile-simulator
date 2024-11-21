e7ef34d4b0e2f8b5b5af34ecaecf1b135feffdb0
HL-2002 <henryslang8231682@gmail.com>
2024-11-21 15:35:32 -0400

    Add open apps to footer

---
+++ src/components/AppTabs.tsx
@@ -0,0 +1,24 @@
+import useMovilStore from "@stores/movil";
+import { memo } from "react";
+
+function AppTabs() {
+    const process = useMovilStore((state) => state.process)
+    const changePage = useMovilStore((state) => state.changePage)
+
+    return (
+        <div className="flex justify-between">
+            {
+                process.map((p) => {
+                    return (
+                        <div key={p.name} onClick={() => changePage(p.name)}
+                            className="flex cursor-pointer m-1" >
+                            <img src={p.urlIcon} alt={p.name} className="w-8"/>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default memo(AppTabs)
--- src/components/OSFooter.tsx
+++ src/components/OSFooter.tsx
@@ -1,19 +1,12 @@
-import useMovilStore from '@stores/movil.ts'
 import Home from './Home.tsx'
-import {Square} from 'lucide-react'
+import AppTabs from './AppTabs.tsx'
 
 
 export default function OSFooter () {
-    const setCurrentPage = useMovilStore((state) => state.changePage)
-
-
     return (
-        <footer className="bg-slate-50 flex items-center justify-center z-10 gap-3">
-            <button onClick={() => setCurrentPage('Navigation')}>
-            <Square size={20} className="m-1" />
-            </button>
-
+        <footer className="bg-slate-50 flex pl-2 z-10 h-10">
             <Home />
+            <AppTabs/>
         </footer>
     )
 }
